ed4d8b92d2637090079161513db18fbaa0e65930
ivan <liangyichen828@yahoo.com>
2016-08-26 00:09:08 +0800

    add new feature for account email

---
+++ src/main/java/com/ivan/mvnbook/account/email/AccountEmailException.java
@@ -0,0 +1,11 @@
+package com.ivan.mvnbook.account.email;
+
+import javax.mail.MessagingException;
+
+public class AccountEmailException extends Exception {
+
+	public AccountEmailException(String string, MessagingException e) {
+		
+	}
+
+}
