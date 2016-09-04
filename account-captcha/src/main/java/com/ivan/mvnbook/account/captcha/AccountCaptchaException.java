c06bf14040f3f8f479ee933119d13c15f00f4df0
ivan <liangyichen828@yahoo.com>
2016-09-04 15:32:59 +0800

    api change and add a new module

---
+++ account-captcha/src/main/java/com/ivan/mvnbook/account/captcha/AccountCaptchaException.java
@@ -0,0 +1,15 @@
+package com.ivan.mvnbook.account.captcha;
+
+import java.io.IOException;
+
+public class AccountCaptchaException extends Exception {
+
+	public AccountCaptchaException(String string) {
+		
+	}
+
+	public AccountCaptchaException(String string, Exception e) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
